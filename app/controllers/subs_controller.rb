b42d859c8687522b63efcd47eff5b247409fda94
Lincxx <bossco28@gmail.com>
2021-12-30 19:33:38 -0500

    Add setup routes

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -3,4 +3,17 @@ class SubsController < ApplicationController
     def index
         render component: "Subs"
     end
+
+    def show
+        render component: "Sub"
+    end
+
+    def edit
+        render component: "SubEdit"
+    end
+
+    def new
+        render component: "SubNew"
+    end
 end
+
