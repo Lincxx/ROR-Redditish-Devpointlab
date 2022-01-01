7da84e50b07fcc354ae1b54dfbb1649420459075
Lincxx <bossco28@gmail.com>
2022-01-01 09:30:37 -0500

    sub show and set sub method

--- app/javascript/components/Sub.js
+++ app/javascript/components/Sub.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React from "react"
 
 const Sub = (props) => {
+    const { id, name, created_at } = props.sub
     return(
-        <div>
+        <>
             <h1>Sub component here</h1>
-        </div>
+            <h3>{name}</h3>
+            <p>
+                created: {created_at}, id: {id}
+            </p>
+            <a href="/">back</a>
+        </>
     )
 }
 
